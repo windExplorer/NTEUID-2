41967a198bb83143c2289bee0660b4a98309d661
云端之风 <1479221500@qq.com>
2026-07-03 13:08:42 +0800

    fix: 资源下载后重载角色名索引
    下载全部资源命令完成后调用 reload_all() 刷新 CHARS/FORKS 内存索引，
    避免 char_meta.json 更新后查询仍返回无此角色

--- NTEUID/nte_resource/__init__.py
+++ NTEUID/nte_resource/__init__.py
@@ -3,6 +3,7 @@ from gsuid_core.bot import Bot
 from gsuid_core.models import Event
 
 from ..utils.resource.git_resource import update_resources
+from ..utils.name_convert import reload_all
 
 sv_nte_resource = SV("nte资源", pm=1)
 
@@ -11,4 +12,5 @@ sv_nte_resource = SV("nte资源", pm=1)
 async def send_update_resource_msg(bot: Bot, ev: Event):
     await bot.send("[异环] 正在开始下载~可能需要较久的时间!")
     result = await update_resources(is_force=True)
+    reload_all()
     await bot.send(f"[异环] {result['message']}")
